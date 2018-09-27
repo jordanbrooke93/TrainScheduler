65428bb7a35728ab4086807dfca2e6fb825f6caf
jordanbrooke93 <jordantaylor93@me.com>
2018-09-26 21:21:09 -0400

    working complete code

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -17,8 +17,9 @@ $("#submitButton").on("click", function (event) {
     var destination = $("#destination").val().trim()
     var time = $("#time").val().trim()
     var frequency = $("#frequency").val().trim()
+    console.log(time)
 
-   
+
 
     $(".form-control").val("")
 
@@ -36,9 +37,31 @@ database.ref().on("child_added", function (snapshot) {
     var name = snapshot.val().name
     var destination = snapshot.val().destination
     var frequency = snapshot.val().frequency
+    var time = snapshot.val().time
+    console.log(time)
 
-    var displayRow = $("<tr><td>" + name + "</td><td>" + destination + "</td><td>" + frequency + "</td></tr>")
+    var timeConverted = moment(time, "HH:mm").subtract(1, "years")
+    console.log(timeConverted)
+
+    var currentTime = moment()
+    console.log("current time " + moment(currentTime).format("HH:mm"))
+
+    var timeDifference = currentTime.diff(timeConverted, "minutes")
+    console.log("time difference " + timeDifference)
+
+    var remainder = timeDifference % frequency
+    console.log(remainder)
+
+    var minutesAway = frequency - remainder;
+    console.log("minutes away " + minutesAway)
+
+    var nextArrival = currentTime.add(minutesAway, "HH:mm")
+    console.log("next arrival: " + moment(nextArrival).format("HH:mm"));
+
+    var displayRow = $("<tr><td>" + name + "</td><td>" + destination + "</td><td>" + frequency + "</td><td>" + nextArrival.format("HH:mm") + "</td><td>" + minutesAway + "</td></tr>")
     $("#tbody").append(displayRow)
+
 
 }, function (errorObjects) {
 })
+
